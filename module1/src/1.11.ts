96e1897964370e447604d620d53b5fc7734132f4
Araf <arafathossain.baiust@gmail.com>
2023-11-02 07:33:46 +0600

    1.11.ts added

---
+++ module1/src/1.11.ts
@@ -0,0 +1,20 @@
+{
+    // ternary operation
+    const age: number = 15;
+    if (age >=18){
+        console.log('Adult');
+    }else{
+        console.log('Not Adult');
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
